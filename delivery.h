20c73d48c22a61113c221b04f4cef719aa6e029e
Alef Cauan Sousa Rodriguês <149737667+alefCauan@users.noreply.github.com>
2024-07-11 14:34:23 -0300

    start

---
+++ delivery.h
@@ -0,0 +1,42 @@
+// informações do cliente
+typedef struct client {
+    
+}Client;
+
+typedef struct deliveries_node {
+    /* data */
+}Deliveries_node;
+// pilha de entregas não efetuadas
+typedef struct deliveries {
+    /* data */
+}Deliveries;
+
+typedef struct devolution_node {
+    /* data */
+}Devolution_node;
+// fila de devoluções 
+typedef struct devolution {
+    /* data */
+}Devolution;
+
+typedef struct route_node {
+    /* data */
+}Route_node;
+// fila de rotas de entregas
+typedef struct route {
+    /* data */
+}Route;
+
+////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////// DEFINES ///////////////////////////////////////
+#define DELIVERY_FIRST 5 // fez a entrega de primeira
+#define DELIVERY_SECOND 3 // fez a entrega de segunda 
+#define DELIVERY_DEVOLUTION -1 // não fez a entrega 
+
+
+////////////////////////////////////////////////////////////////////////////////
+//////////////////// FUNÇÕES DE ALOCAÇÃO/DESALOCAÇÃO ///////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////////
+//////////////////// FUNÇÕES DE ALOCAÇÃO/DESALOCAÇÃO ///////////////////////////
